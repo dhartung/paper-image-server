580c180a0f226ec93a1bd49ecba4e08a5a6037f1
Daniel Hartung <daniel.hartung@codecentric.de>
2024-01-03 15:00:31 +0100

    Add device key

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import express, { Application } from "express";
 import "dotenv/config";
 import { getNextImage } from "./image-selector";
 import { createMessage, readImageAsBuffer } from "./message-writer";
-import "express-async-errors"
+import "express-async-errors";
 
 const PORT = process.env.PAPER_PORT || 3000;
 const IMAGE_DIRECTORY = process.env.PAPER_IMAGE_DIRECTORY || "./images";
 const SLEEP_TIME_IN_SECONDS = process.env.PAPER_SLEEP_TIME_IN_SECONDS || 60_000;
 const BASE_PATH = process.env.PAPER_BASE_PATH || "/";
+const DEVICE_KEYS = process.env.PAPER_DEVICE_KEYS?.split(",") || [];
 
 const app = express();
 const base = express();
@@ -22,23 +23,27 @@ const toNumber = (value: any): number => {
   }
 };
 
-const toInt = (value: any): number => {
-  const num = toNumber(value);
-  if (!Number.isInteger(num)) {
-    throw Error(`Cannot convert ${value} to integer`);
+const checkDeviceKey = (key: string | undefined): void => {
+  if (DEVICE_KEYS.length > 0) {
+    if (!key || !DEVICE_KEYS.includes(key.toString())) {
+      throw Error(`Invalid key`);
+    }
   }
-  return num;
 };
 
 base.get("/", async (req, res) => {
-  const { version, voltage, wakups } = req.query;
-  console.log(
-    `Got request with version: ${version}, voltage: ${voltage}, wakups: ${wakups}`
-  );
+  const { version, voltage, wakeups, key } = req.query;
+  console.log(`Got request with parameter: ${JSON.stringify(req.query)}`);
 
-  const fileName = await getNextImage(IMAGE_DIRECTORY, toInt(version || 0));
+  checkDeviceKey(key?.toString());
+
+  const fileName = await getNextImage(IMAGE_DIRECTORY, toNumber(version || 0));
   const image = await readImageAsBuffer(fileName.fileName);
-  const response = await createMessage(image, fileName.id, toInt(SLEEP_TIME_IN_SECONDS));
+  const response = await createMessage(
+    image,
+    fileName.id,
+    toNumber(SLEEP_TIME_IN_SECONDS)
+  );
 
   res.setHeader("Content-Type", "application/octet-stream");
   res.send(response);
@@ -47,8 +52,9 @@ base.get("/", async (req, res) => {
 app.use(BASE_PATH, base);
 
 if (BASE_PATH != "/") {
-    app.get("/", (_, res) => res.redirect(301, BASE_PATH))
+  app.get("/", (_, res) => res.redirect(301, BASE_PATH));
 }
 
-
-app.listen(toInt(PORT), () => console.log("Server Running on Port " + PORT + " and path " + BASE_PATH));
+app.listen(toNumber(PORT), () =>
+  console.log("Server Running on Port " + PORT + " and path " + BASE_PATH)
+);
